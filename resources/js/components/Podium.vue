b4a09f6be8d2efcddfbb1aab0f569ed93a7f739b
abdennour <terfaniahmedabdelnour@gmail.com>
2023-05-02 20:48:01 +0100

    page RH/Classement not finished yet

--- resources/js/components/Podium.vue
+++ resources/js/components/Podium.vue
@@ -1,10 +1,13 @@
 <template>
     <div
         :class="
-            'relative h-48 flex items-center bg-gradient-to-t rounded-3xl py-7 w-full px-4 text-vN mb-10 border-[#EEEEF2] ' +
-            bg
+            'relative h-48 flex items-center bg-gradient-to-t rounded-3xl py-7 w-full px-4 text-vN mb-10 border-[#EEEEF2] '+bg
         "
-    >
+    >   <div v-if="position == 1"
+        class="mb-[200px]"
+        >
+
+        </div>
         <div class="flex flex-col h-3/4 items-start justify-between">
             <h1 class="uppercase text-2xl font-bold text-vN">
                 {{ name }}
@@ -29,7 +32,7 @@
             >
                 <img src="/assets/logo/IEC_WHITE.png" />
             </div>
-
+            
             <div
                 v-if="position == 1"
                 class="z-0 absolute h-12 w-12 -top-16 -right-16 rotate-45 p-2"
@@ -38,13 +41,13 @@
             </div>
             <div
                 v-if="position == 2"
-                class="z-100 absolute h-12 w-12 top-6 -right-6 mt-[10px]"
+                class="z-100 absolute h-12 w-12 top-6 -right-6 "
             >
                 <img src="/assets/icons/second.svg" alt="second place icon" />
             </div>
             <div
                 v-if="position == 3"
-                class="z-100 absolute h-14 w-14 top-2 -right-6 mt-[10px]"
+                class="z-100 absolute h-14 w-14 top-2 -right-6"
             >
                 <img src="/assets/icons/3rd.svg" alt="third place icon" />
             </div>
